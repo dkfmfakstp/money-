850dd6021d9c563bc5477f317a3025cc260fef56
dkfmfakstp <ksj20070917@gmail.com>
2025-06-09 12:33:37 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+uploaded_file = st.file_uploader("CSV 파일을 업로드하세요", type=["csv"])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head())
+else:
+    st.write("CSV 파일을 업로드해주세요.")
+
+uploaded_file = list(uploaded_file)
+money={}
+print(uploaded_file)
